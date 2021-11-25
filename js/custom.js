a89d65b1e836deb8b9419b303b4ec64f3a7e5e0d
Mostafaemad228 <42499279+Mostafaemad228@users.noreply.github.com>
2021-11-25 12:43:03 +0200

    commit

---
+++ js/custom.js
@@ -0,0 +1,66 @@
+
+  // /////////////////////////////////////////
+  window.addEventListener("scroll" , function () {    
+    var navbar =  document.querySelector("nav");
+    navbar.classList.toggle("sticky",window.scrollY >100);
+  })
+  // ///////////////////////////////////////////////////////////// ///////////////////////
+ /* 1. Proloder */
+$(window).on('load', function () {
+    $('#preloader-active').delay(450).fadeOut('slow');
+    $('body').delay(850).css({
+    'overflow': 'visible'
+    });
+});
+// ////////////////////////// /////////////////////// ///////////////////////
+var typed = new Typed(".type-text", {
+  strings: [
+    "ceo devfolio",
+    "_the center of attention.",
+    "_Get Pretty Look",
+    "_the beauty of the woman.",],
+  typeSpeed: 100,
+  backSpeed: 50,
+  // smartBackspace: true,
+  loop: false,
+  loopCount: Infinity,
+});
+
+// ///////////////////////////////////////////////////////////////////////////
+
+  
+  
+
+
+  
+  
+     
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
